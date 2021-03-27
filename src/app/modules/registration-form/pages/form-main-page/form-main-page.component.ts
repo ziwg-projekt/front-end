81d558a81d4045289aa35c6e27d3c1cb3a707b4f
karo99 <urbanska.karolina11@gmail.com>
2021-03-27 19:43:56 +0100

    news section updated

--- src/app/modules/registration-form/pages/form-main-page/form-main-page.component.ts
+++ src/app/modules/registration-form/pages/form-main-page/form-main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-form-main-page',
@@ -6,8 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-main-page.component.scss']
 })
 export class FormMainPageComponent implements OnInit {
+  public newsArray: {
+    date: string,
+    label: string,
+    labelExtension: string
+  }[];
 
-  constructor() { }
+  constructor() {
+    this.newsArray = [
+      {
+        date: '10.03',
+        label: 'MZ: Bardzo dużo nowych zakażeń',
+        labelExtension: 'Szczegóły raportu MZ'
+      },
+      {
+        date: '04.03',
+        label: 'Znamy termin dostawy',
+        labelExtension: 'Nowa partia szczepionek wkrótce'
+      },
+      {
+        date: '28.02',
+        label: 'Ostrzeżenie',
+        labelExtension: 'Nowa mutacja koronawirusa w Polsce'
+      },
+      {
+        date: '13.02',
+        label: 'Zaczynamy szczepić 70-latków',
+        labelExtension: 'Kolejna grupa wiekowa'
+      }
+    ];
+  }
 
   ngOnInit(): void {
   }
